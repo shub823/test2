c05ecf72cef208f33434f775814f3326b5adad60
shub823 <skagarwal8235@gmail.com>
2020-09-12 16:32:28 +0530

    xml

---
+++ src/main/java/com/lti/CircleService.java
@@ -0,0 +1,18 @@
+package com.lti;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/circle")
+public class CircleService {
+	
+	
+	@GET
+	@Path("/area/{radius}")
+    public String getArea(@PathParam(value = "radius" ) double radius) {
+		double area= Math.PI*radius*radius;
+		return String.valueOf(area);
+    	
+    }
+}
